dd584de2eeca6c78e040bd6b1f9abfd952a775b2
Douglas Hill <hello@douglashill.co>
2019-12-05 23:49:49 +0000

    Add support for table views with multiple sections

--- KeyboardKit/KeyboardTableView.swift
+++ KeyboardKit/KeyboardTableView.swift
@@ -4,7 +4,6 @@
 import UIKit
 
 /// A table view that allows navigation and selection using a hardware keyboard.
-/// Only supports a single section.
 public class KeyboardTableView: UITableView {
     // These properties may be set or overridden to provide discoverability titles for key commands.
     public var selectAboveDiscoverabilityTitle: String?
@@ -30,37 +29,154 @@ public class KeyboardTableView: UITableView {
     }
 
     @objc private func selectAbove() {
-        if let oldSelectedIndexPath = indexPathForSelectedRow {
-            selectRowAtIndex(oldSelectedIndexPath.row - 1)
+        if let oldSelection = indexPathForSelectedRow, let target = selectableIndexPathBeforeIndexPath(oldSelection) {
+            selectAndShowRowAtIndexPath(target)
         } else {
             selectBottom()
         }
     }
 
     @objc private func selectBelow() {
-        if let oldSelectedIndexPath = indexPathForSelectedRow {
-            selectRowAtIndex(oldSelectedIndexPath.row + 1)
+        if let oldSelection = indexPathForSelectedRow, let target = selectableIndexPathAfterIndexPath(oldSelection) {
+            selectAndShowRowAtIndexPath(target)
         } else {
             selectTop()
         }
     }
 
     @objc private func selectTop() {
-        selectRowAtIndex(0)
+        if let indexPath = firstSelectableIndexPath {
+            selectAndShowRowAtIndexPath(indexPath)
+        }
     }
 
     @objc private func selectBottom() {
-        selectRowAtIndex(numberOfRows(inSection: 0) - 1)
+        if let indexPath = lastSelectableIndexPath {
+            selectAndShowRowAtIndexPath(indexPath)
+        }
     }
 
-    /// Tries to select and scroll to the row at the given index in section 0.
-    /// Does not require the index to be in bounds. Does nothing if out of bounds.
-    private func selectRowAtIndex(_ rowIndex: Int) {
-        guard rowIndex >= 0 && rowIndex < numberOfRows(inSection: 0) else {
-            return
+    private var delegateRespondsToShouldHighlightRow: Bool {
+        delegate?.responds(to: #selector(UITableViewDelegate.tableView(_:shouldHighlightRowAt:))) ?? false
+    }
+
+    private func uncheckedShouldHighlightRowAtIndexPath(_ indexPath: IndexPath) -> Bool {
+        delegate!.tableView!(self, shouldHighlightRowAt: indexPath)
+    }
+
+    private func checkIndexPathIsInValidRange(_ indexPath: IndexPath) {
+        precondition(indexPath.section >= 0, "Index path is out-of-bounds.")
+        precondition(indexPath.section < numberOfSections, "Index path is out-of-bounds.")
+        precondition(indexPath.row >= 0, "Index path is out-of-bounds.")
+        precondition(indexPath.row < numberOfRows(inSection: indexPath.section), "Index path is out-of-bounds.")
+    }
+
+    /// Returns the index path to select before (above) a given index path or nil if there is no such index path.
+    private func selectableIndexPathBeforeIndexPath(_ indexPath: IndexPath) -> IndexPath? {
+        checkIndexPathIsInValidRange(indexPath)
+        let responds = delegateRespondsToShouldHighlightRow
+
+        var section = indexPath.section
+        while section >= 0 {
+            let numberOfRows_ = numberOfRows(inSection: section)
+            // For the first section we look in, we want to just check the row above in the same section.
+            // When the section changes, we need to start from the last row.
+            var row = section == indexPath.section ? indexPath.row - 1 : numberOfRows_ - 1
+
+            while row >= 0 {
+                let targetIndexPath = IndexPath(row: row, section: section)
+                if responds == false || uncheckedShouldHighlightRowAtIndexPath(targetIndexPath) {
+                    return targetIndexPath
+                }
+
+                row -= 1
+            }
+
+            section -= 1
         }
 
-        let indexPath = IndexPath(row: rowIndex, section: 0)
+        return nil
+    }
+
+    /// Returns the index path to select after (below) a given index path or nil if there is no such index path.
+    private func selectableIndexPathAfterIndexPath(_ indexPath: IndexPath) -> IndexPath? {
+        checkIndexPathIsInValidRange(indexPath)
+        let responds = delegateRespondsToShouldHighlightRow
+
+        var section = indexPath.section
+        while section < numberOfSections {
+            let numberOfRows_ = numberOfRows(inSection: section)
+            // For the first section we look in, we want to just check the row below in the same section.
+            // When the section changes, we need to start from the first row.
+            var row = section == indexPath.section ? indexPath.row + 1 : 0
+
+            while row < numberOfRows_ {
+                let targetIndexPath = IndexPath(row: row, section: section)
+                if responds == false || uncheckedShouldHighlightRowAtIndexPath(targetIndexPath) {
+                    return targetIndexPath
+                }
+
+                row += 1
+            }
+
+            section += 1
+        }
+
+        return nil
+    }
+
+    private var firstSelectableIndexPath: IndexPath? {
+        let responds = delegateRespondsToShouldHighlightRow
+
+        // Select the first highlightable row.
+        var section = 0
+        while section < numberOfSections {
+            let numberOfRows_ = numberOfRows(inSection: section)
+
+            var row = 0
+            while row < numberOfRows_ {
+                let targetIndexPath = IndexPath(row: row, section: section)
+                if responds == false || uncheckedShouldHighlightRowAtIndexPath(targetIndexPath) {
+                    return targetIndexPath
+                }
+
+                row += 1
+            }
+
+            section += 1
+        }
+
+        return nil
+    }
+
+    private var lastSelectableIndexPath: IndexPath? {
+        let responds = delegateRespondsToShouldHighlightRow
+
+        // Select the last highlightable row.
+        var section = numberOfSections - 1
+        while section >= 0 {
+            let numberOfRows_ = numberOfRows(inSection: section)
+
+            var row = numberOfRows_ - 1
+            while row >= 0 {
+                let targetIndexPath = IndexPath(row: row, section:section)
+                if responds == false || uncheckedShouldHighlightRowAtIndexPath(targetIndexPath) {
+                    return targetIndexPath
+                }
+
+                row -= 1
+            }
+
+            section -= 1
+        }
+
+        return nil
+    }
+
+    /// Tries to select and scroll to the row at the given index path.
+    /// The index path is expected to be in-bounds.
+    private func selectAndShowRowAtIndexPath(_ indexPath: IndexPath) {
+        checkIndexPathIsInValidRange(indexPath)
 
         switch cellVisibility(atIndexPath: indexPath) {
         case .fullyVisible:
--- KeyboardKitDemo/SimpleListViewController.swift
+++ KeyboardKitDemo/SimpleListViewController.swift
@@ -29,13 +29,21 @@ class SimpleListViewController: UIViewController, UITableViewDataSource, UITable
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Section \(numberFormatter.string(from: NSNumber(value: section + 1))!)"
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 101
+        return 33
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
-        cell.textLabel?.text = numberFormatter.string(from: NSNumber(value: indexPath.row))
+        cell.textLabel?.text = numberFormatter.string(from: NSNumber(value: indexPath.row + 1))
         return cell
     }
 
