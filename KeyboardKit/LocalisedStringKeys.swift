af27a8dfda5828250b7a860e6b74d360b7bc36d5
Douglas Hill <hello@douglashill.co>
2019-12-22 18:20:29 +0000

    Add generated string keys file and add resources for CocoaPods
    Since CocoaPods basically sets up its own build system the generation script can’t run. Therefore check this file in even though it’s generated.

---
+++ KeyboardKit/LocalisedStringKeys.swift
@@ -0,0 +1,17 @@
+// This file was automatically generated from UpdateLocalisedStringKeys.swift.
+
+enum LocalisedStringKey: String {
+    case app_newWindow
+    case app_settings
+    case find_jump
+    case find_next
+    case find_previous
+    case find_useSelection
+    case navigationController_back
+    case scrollView_refresh
+    case scrollView_zoomIn
+    case scrollView_zoomOut
+    case scrollView_zoomReset
+    case window_close
+    case window_cycle
+}
