0ebf46d4af87d87a18a8c1ad4ef5daceab1415c6
Douglas Hill <hello@douglashill.co>
2020-01-05 18:50:28 +0000

    ✅ Fix text input conflict detection with capital letters

--- KeyboardKit/TextInput.swift
+++ KeyboardKit/TextInput.swift
@@ -68,6 +68,11 @@ extension UIKeyCommand {
         case .pageUp, .pageDown, .home, .end:
             // These are never used for text input.
             return false
+        default:
+            break
+        }
+        // Normalise case because the system ignores the case. Can’t do this at the start because lowercasing strings like UIKeyInputUpArrow makes them not match.
+        switch (input.lowercased()) {
         case "a", "b", "d", "e", "f", "h", "i", "j", "k", "m", "n", "o", "p", "t":
             // http://www.hcs.harvard.edu/~jrus/Site/system-bindings.html minus some that don’t work on iOS, plus some that do something on iOS but not on Mac.
             // These do something useful in text input with control or shift + control. Command or control + option are not used for text input.
