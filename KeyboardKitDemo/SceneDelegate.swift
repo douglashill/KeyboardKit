8f4438970da95237a1c8fbf0c92225c5ec99f9d6
Douglas Hill <hello@douglashill.co>
2020-03-15 21:47:02 +0000

    Avoid the keyboard bar or software keyboard in demo app

--- KeyboardKitDemo/SceneDelegate.swift
+++ KeyboardKitDemo/SceneDelegate.swift
@@ -22,5 +22,33 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = tabBarController
         window.makeKeyAndVisible()
         self.window = window
+
+        NotificationCenter.default.addObserver(self, selector: #selector(updateSafeAreaForKeyboardFromNotification), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateSafeAreaForKeyboardFromNotification), name: UIResponder.keyboardDidChangeFrameNotification, object: nil)
+    }
+
+    /// Deal with the avoiding the software keyboard globally (or more likely the bar that’s shown when a hardware keyboard is connected).
+    /// This is needed for the TextViewController tab, and also if another app in Split View is showing the keyboard.
+    /// Taken from https://gist.github.com/douglashill/41ea84f0ba59feecd3be51f21f73d501
+    @objc private func updateSafeAreaForKeyboardFromNotification(_ notification: Notification) {
+        guard
+            let endFrameInScreenCoords = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+            let window = window,
+            let viewController = window.rootViewController
+        else {
+            return
+        }
+
+        let view = viewController.view!
+        let endFrameInSelfCoords = view.convert(endFrameInScreenCoords, from: window.screen.coordinateSpace)
+
+        // Need to clear the additionalSafeAreaInsets in order to be able to read the unaltered safeAreaInsets. We’ll set it again just below.
+        viewController.additionalSafeAreaInsets = .zero
+        let safeBounds = view.bounds.inset(by: view.safeAreaInsets)
+
+        let isDocked = endFrameInSelfCoords.maxY >= safeBounds.maxY
+        let keyboardOverlapWithViewFromBottom = isDocked ? max(0, safeBounds.maxY - endFrameInSelfCoords.minY) : 0
+
+        viewController.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardOverlapWithViewFromBottom, right: 0)
     }
 }
