3be8ae06798212c5f6e5268802eb53020dc3d8f4
Douglas Hill <hello@douglashill.co>
2020-06-14 10:01:58 +0100

    Rename Localizable.strings to KeyboardKit.strings
    When working on an app it‘s annoying having these show up in Xcode Quick Open when you probably want Localizable.strings from the app.

--- KeyboardKit/LocalisedStrings.swift
+++ KeyboardKit/LocalisedStrings.swift
@@ -5,7 +5,7 @@ import Foundation
 private class Marker {}
 private let keyboardKitBundle = Bundle(for: Marker.self)
 
-/// Looks up a string in the KeyboardKit Localizable.strings file. The cases of the parameter type are generated from the English strings file.
+/// Looks up a string in KeyboardKit.strings files. The cases of the parameter type are generated from the English strings file.
 func localisedString(_ key: LocalisedStringKey) -> String {
-    return keyboardKitBundle.localizedString(forKey: key.rawValue, value: nil, table: nil)
+    return keyboardKitBundle.localizedString(forKey: key.rawValue, value: nil, table: "KeyboardKit")
 }
