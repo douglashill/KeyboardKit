4f90b42c3e73c5edbd35ed2b9ec8041e83c5dbb3
Seb Jachec <sjachec@me.com>
2020-07-06 11:39:57 +0100

    Complete Swift Package changes, update README

--- KeyboardKit/LocalisedStrings.swift
+++ KeyboardKit/LocalisedStrings.swift
@@ -2,8 +2,12 @@
 
 import Foundation
 
-private class Marker {}
-private let keyboardKitBundle = Bundle(for: Marker.self)
+#if SWIFT_PACKAGE
+private let keyboardKitBundle = Bundle.module
+#else
+private class BundleFinder {}
+private let keyboardKitBundle = Bundle(for: BundleFinder.self)
+#endif
 
 /// Looks up a string in KeyboardKit.strings files. The cases of the parameter type are generated from the English strings file.
 func localisedString(_ key: LocalisedStringKey) -> String {
