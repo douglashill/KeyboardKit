ab6e97b0efc56582b52467b68e9bb007a85a3650
Douglas Hill <hello@douglashill.co>
2020-09-10 09:38:04 +0100

    Capture system item with new initialisers

--- KeyboardKit/ObjC/BarButtonItem.m
+++ KeyboardKit/ObjC/BarButtonItem.m
@@ -6,14 +6,30 @@
 #import <KeyboardKit/BarButtonItem.h>
 #endif
 
+NS_ASSUME_NONNULL_BEGIN
+
 @implementation KBDBarButtonItem
 
-- (instancetype)initWithBarButtonSystemItem:(UIBarButtonSystemItem)systemItem target:(id)target action:(SEL)action {
+- (instancetype)initWithBarButtonSystemItem:(UIBarButtonSystemItem)systemItem target:(nullable id)target action:(nullable SEL)action {
     self = [super initWithBarButtonSystemItem:systemItem target:target action:action];
     if (self == nil) return nil;
-
     [self wasInitialisedWithSystemItem:systemItem];
+    return self;
+}
 
+// The two below won’t work anyway because primaryAction and menu aren’t supported, but might as well capture the system item for completeness.
+
+- (instancetype)initWithBarButtonSystemItem:(UIBarButtonSystemItem)systemItem primaryAction:(nullable UIAction *)primaryAction API_AVAILABLE(ios(14.0)) {
+    self = [super initWithBarButtonSystemItem:systemItem primaryAction:primaryAction];
+    if (self == nil) return nil;
+    [self wasInitialisedWithSystemItem:systemItem];
+    return self;
+}
+
+- (instancetype)initWithBarButtonSystemItem:(UIBarButtonSystemItem)systemItem menu:(nullable UIMenu *)menu API_AVAILABLE(ios(14.0)) {
+    self = [super initWithBarButtonSystemItem:systemItem menu:menu];
+    if (self == nil) return nil;
+    [self wasInitialisedWithSystemItem:systemItem];
     return self;
 }
 
@@ -22,3 +38,5 @@
 }
 
 @end
+
+NS_ASSUME_NONNULL_END
