13ec8c2c37994c1acaf52387481556f28bdc7c4e
Douglas Hill <hello@douglashill.co>
2020-12-28 19:37:15 +0000

    Add KeyboardDatePicker

---
+++ KeyboardKit/KeyboardDatePicker.swift
@@ -0,0 +1,83 @@
+// Douglas Hill, December 2020
+
+import UIKit
+
+/// A date picker that supports using a hardware keyboard arrow keys to change the date.
+///
+/// Only changing the day in an inline date or date & time picker is supported.
+@available(iOS 14.0, *)
+open class KeyboardDatePicker: UIDatePicker {
+    public override var canBecomeFirstResponder: Bool {
+        true
+    }
+
+    public override var keyCommands: [UIKeyCommand]? {
+        var commands = super.keyCommands ?? []
+
+        if canGoUp {
+            commands.append(UIKeyCommand(.upArrow, action: #selector(kdb_goUp)))
+        }
+        if canGoDown {
+            commands.append(UIKeyCommand(.downArrow, action: #selector(kdb_goDown)))
+        }
+        if canGoLeft {
+            commands.append(UIKeyCommand(.leftArrow, action: #selector(kdb_goLeft)))
+        }
+        if canGoRight {
+            commands.append(UIKeyCommand(.rightArrow, action: #selector(kdb_goRight)))
+        }
+
+        return commands
+    }
+
+    public override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        switch action {
+        case #selector(kdb_goUp): return canGoUp
+        case #selector(kdb_goDown): return canGoDown
+        case #selector(kdb_goLeft): return canGoLeft
+        case #selector(kdb_goRight): return canGoRight
+        default:
+            return super.canPerformAction(action, withSender: sender)
+        }
+    }
+
+    private var canGoUp: Bool {
+        true
+    }
+
+    @objc private func kdb_goUp(_ sender: AnyObject?) {
+        adjustDate(byAdding: -1, to: .weekOfMonth)
+    }
+
+    private var canGoDown: Bool {
+        true
+    }
+
+    @objc private func kdb_goDown(_ sender: AnyObject?) {
+        adjustDate(byAdding: +1, to: .weekOfMonth)
+    }
+
+    // TODO: Right-to-left support.
+
+    private var canGoLeft: Bool {
+        true
+    }
+
+    @objc private func kdb_goLeft(_ sender: AnyObject?) {
+        adjustDate(byAdding: -1, to: .day)
+    }
+
+    private var canGoRight: Bool {
+        true
+    }
+
+    @objc private func kdb_goRight(_ sender: AnyObject?) {
+        adjustDate(byAdding: +1, to: .day)
+    }
+
+    private func adjustDate(byAdding value: Int, to component: Calendar.Component) {
+        if let adjustedDate = calendar.date(byAdding: component, value: value, to: date) {
+            setDate(adjustedDate, animated: true)
+        }
+    }
+}
---
+++ KeyboardKitDemo/DatePickerViewController.swift
@@ -0,0 +1,39 @@
+// Douglas Hill, December 2020
+
+import UIKit
+import KeyboardKit
+
+class DatePickerViewController: FirstResponderViewController {
+    override init() {
+        super.init()
+        title = "Date Picker"
+        tabBarItem.image = UIImage(systemName: "calendar")
+    }
+
+    private lazy var datePicker = KeyboardDatePicker()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+        view.addSubview(datePicker)
+
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            datePicker.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            datePicker.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            datePicker.widthAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.widthAnchor),
+            datePicker.heightAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.heightAnchor),
+        ])
+
+        datePicker.preferredDatePickerStyle = .inline
+        datePicker.datePickerMode = .date
+        datePicker.minimumDate = datePicker.calendar.date(byAdding: .month, value: -1, to: datePicker.date)
+        datePicker.maximumDate = datePicker.calendar.date(byAdding: .month, value: +1, to: datePicker.date)
+    }
+
+    override var kd_preferredFirstResponderInHierarchy: UIResponder? {
+        presentedViewController ?? datePicker
+    }
+}
