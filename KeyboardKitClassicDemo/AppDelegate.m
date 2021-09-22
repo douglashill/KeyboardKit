0dbbb6ae9091bb3900ae7d7864acd91de81b3b88
Douglas Hill <hello@douglashill.co>
2021-09-22 09:05:57 +0100

    Add tab bar controller to classic demo

--- KeyboardKitClassicDemo/AppDelegate.m
+++ KeyboardKitClassicDemo/AppDelegate.m
@@ -11,11 +11,20 @@
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    let viewController = [[ClassicDemoViewController alloc] init];
-    [viewController setTitle:@"KeyboardKit Classic Demo"];
+    let viewController1 = [[ClassicDemoViewController alloc] init];
+    [viewController1 setTitle:@"Demo 1"];
+
+    let viewController2 = [[ClassicDemoViewController alloc] init];
+    [viewController2 setTitle:@"Demo 2"];
+
+    let tabBarController = [[KeyboardTabBarController alloc] init];
+    tabBarController.viewControllers = @[
+        [[KeyboardNavigationController alloc] initWithRootViewController:viewController1],
+        [[KeyboardNavigationController alloc] initWithRootViewController:viewController2],
+    ];
 
     let window = [[KeyboardWindow alloc] init];
-    [window setRootViewController:[[KeyboardNavigationController alloc] initWithRootViewController:viewController]];
+    [window setRootViewController:tabBarController];
     [window makeKeyAndVisible];
     [self setWindow:window];
 
