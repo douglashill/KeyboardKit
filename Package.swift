492fb5ac288d8f32110291a1a694ae0de9ed6ab1
Douglas Hill <hello@douglashill.co>
2022-06-07 11:33:58 +0100

    Revert "Add configuration for Swift Package Index DocC"
    This mostly reverts 6cf1c55738c9aead6c15185500224f3d921b60de.
    
    That resulted in a build failure: https://swiftpackageindex.com/builds/7D5EE84B-44B7-4CD3-A1D1-8588EC53BD0C
    
    Reverting until I can look into this.

--- Package.swift
+++ Package.swift
@@ -33,11 +33,3 @@ let package = Package(
         ),
     ]
 )
-
-// This isn’t actually a dependency of KeyboardKit, but Swift Package Index needs this to generate documentation, so we have to add it for everyone.
-// https://blog.swiftpackageindex.com/posts/auto-generating-auto-hosting-and-auto-updating-docc-documentation/
-#if swift(>=5.6)
-  package.dependencies.append(
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-  )
-#endif
