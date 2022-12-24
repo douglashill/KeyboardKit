ce103cb50386f69b9fd4ceb6ad9cbbc5bcad215a
Douglas Hill <hello@douglashill.co>
2022-12-24 12:35:38 +0000

    Make map view work
    Yay! That was easy.

--- KeyboardKit/KeyboardMapView.swift
+++ KeyboardKit/KeyboardMapView.swift
@@ -3,5 +3,15 @@
 import MapKit
 
 /// A map view that supports hardware keyboard commands to scroll and zoom.
+///
+/// This relies on system support that was added in iOS 15.
 open class KeyboardMapView: MKMapView {
+
+    open override var canBecomeFirstResponder: Bool {
+        true
+    }
+
+    open override var canBecomeFocused: Bool {
+        true
+    }
 }
